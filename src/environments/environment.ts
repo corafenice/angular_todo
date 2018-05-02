e24aa8bc4dab22384ad14865fdba9ffa86de4b19
Cora Quezada <corafenice1013@gmail.com>
2018-05-02 16:24:37 -0400

    first commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase : {
+    apiKey: "AIzaSyB7kcedNsyRU-ljrl0qEQdlDsXU4dd90js",
+    authDomain: "angular-todolist-d521f.firebaseapp.com",
+    databaseURL: "https://angular-todolist-d521f.firebaseio.com",
+    projectId: "angular-todolist-d521f",
+    storageBucket: "angular-todolist-d521f.appspot.com",
+    messagingSenderId: "164587613752"
+  }
 };
